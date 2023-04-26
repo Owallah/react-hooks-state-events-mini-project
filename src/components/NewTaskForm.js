cb17d7bd89927195ad59ad8453cbc0ae3ee48f5c
Johnson <mikesonowallah@gmail.com>
2023-04-26 13:46:01 +0300

    Completed assignment

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories, onTaskFormSubmit}) {
+
+  const [text, setText] = useState('')
+  const [category, setCategory] = useState([0])
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newTask = { text, category };
+    onTaskFormSubmit(newTask);
+    setText('');
+    setCategory(categories[0]);
+  };
+
+  const categoryOptions = categories
+    .filter((category) => category !== 'All')
+    .map((category) => (
+      <option key={category} value={category}>
+        {category}
+      </option>
+    ));
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={text} onChange={handleTextChange} />
       </label>
       <label>
         Category
-        <select name="category">
+        <select name="category"  onChange={handleCategoryChange}>
           {/* render <option> elements for each category here */}
+          {categoryOptions}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,28 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks, onTaskDelete}) {
+  console.log(`Tasklist: tasks: ${tasks}`);
+
+  const handleDelete = (value) => {
+    onTaskDelete(tasks.filter((task) => task.text !== value))
+  }
+
+  const taskItem = tasks.map(task => {
+    console.log(`taskItem: ${[task]}`);
+    return (
+      
+          <Task key={task.text} text = {task.text} category={task.category} task={task} onTaskDelete={handleDelete} />
+    )
+    
+  })
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      
+        {taskItem}
+      
     </div>
   );
 }
